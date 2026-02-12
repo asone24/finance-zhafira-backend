99c9a292e78ca17dc23974540df0ef1ff29a428a
asone24 <kucinglucu24kh@gmail.com>
2026-02-12 09:00:13 +0700

    connect database

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const { Pool } = require("pg");
+require("dotenv").config();
+
 const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Finance Zhafira Backend Running");
+});
+
+// Test koneksi database
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
